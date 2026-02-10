46beedae05ca0c89e8451c4f14668493a6b48d51
nolorubio23 <mcarsan165@g.educaand.es>
2026-02-10 08:45:15 +0100

    Commit obligado

--- frontend/src/app/pages/ranking/ranking.ts
+++ frontend/src/app/pages/ranking/ranking.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ranking',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './ranking.html',
   styleUrl: './ranking.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Ranking {
 
